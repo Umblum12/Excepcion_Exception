5aeb2aa00299b252852a705acbfd06dec3a3e995
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-09-28 22:42:00 -0500

    Initial commit

---
+++ Excepcion_Exception/Program.cs
@@ -0,0 +1,30 @@
+﻿using Excepcion_Forma_T_Exception;
+
+namespace Excepcion_Forma_T_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorUno = 0, valorDos = 0, resultado;
+
+            try
+            {
+                Console.Write("Numero 1: ");
+            valorUno = int.Parse(Console.ReadLine());
+            Console.Write("Numero 2: ");
+            valorDos = int.Parse(Console.ReadLine());
+           
+                resultado = valorUno / valorDos;
+                Console.WriteLine(resultado);
+            } catch(Exception ex)
+            { 
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.WriteLine("Final");
+            Console.WriteLine();
+        }
+    }
+}
